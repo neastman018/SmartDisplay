81a3185c081fc2e07da6aed35737df39b3590bdf
neastman018 <neastman018@vt.edu>
2024-06-25 12:55:39 -0400

    update to rotary encoder

--- backend/app/encoder/encoder.py
+++ backend/app/encoder/encoder.py
@@ -1,21 +1,38 @@
-"""
-Class for a rotary encoder
-"""
+import time
 
-import Encoder
-from time import sleep
-import RPi.GPIO as GPIO
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(20, GPIO.IN)
-GPIO.setup(21, GPIO.IN)
-
-encoder = Encoder.Encoder(20, 21)
+from rotary_irq_rp2 import RotaryIRQ
 
 
-if __name__ == "__main__":
-    while True:
-        print(encoder.read())
-        sleep(0.1)
+
+r = RotaryIRQ(pin_num_clk=20,
+
+              pin_num_dt=21,
+
+              min_val=0,
+
+              reverse=True,
+
+              pull_up=True,
+
+              invert=False,
+
+              range_mode=RotaryIRQ.RANGE_UNBOUNDED)
+
+
+
+old_value = r.value()
+
+
+
+while True:
+
+    new_value = r.value()
+
+    if new_value != old_value:
+
+        old_value = new_value
+
+        print(f'Value = {new_value}')
+
+    time.sleep(0.5)
 
