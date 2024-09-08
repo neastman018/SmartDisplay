2f3e786f12132f693b75b62c5d038c682537a469
neastman018 <neastman018@vt.edu>
2024-09-08 14:19:07 -0400

    startup update

--- startup.py
+++ startup.py
@@ -1,6 +1,17 @@
 from subprocess import run
+from threading import Thread
 
+def run_script(script_name):
+    run(["python", script_name])
 
-run("WAYLAND_DISPLAY='wayland-1' wlr-randr --output HDMI-A-1 --transform 270", shell=True)
-run("python3 backend/app/main.py", shell=True)
-run("cd frontend && npm run start", shell=True)
+if __name__ == "__main__":
+
+    run("WAYLAND_DISPLAY='wayland-1' wlr-randr --output HDMI-A-1 --transform 270", shell=True)
+
+    t1 = Thread(target=run_script, args=("backend/app/main.py",))
+    t2 = Thread(target=run_script, args=("frontend/node_modules/.bin/react-scripts start",))
+
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
