fa5ee86b7708e39241b9760bb80ed5c02cc0b9c9
neastman018 <neastman018@vt.edu>
2024-09-08 14:23:25 -0400

    startup update

--- startup.py
+++ startup.py
@@ -2,13 +2,13 @@ from subprocess import run
 from threading import Thread
 
 def run_script(script_name):
-    run(["python", script_name])
+    run(script_name, shell=True)
 
 if __name__ == "__main__":
 
     run("WAYLAND_DISPLAY='wayland-1' wlr-randr --output HDMI-A-1 --transform 270", shell=True)
 
-    t1 = Thread(target=run_script, args=("backend/app/main.py",))
+    t1 = Thread(target=run_script, args=("python backend/app/main.py",))
     t2 = Thread(target=run_script, args=("cd frontend && npm run start",))
 
     t1.start()
