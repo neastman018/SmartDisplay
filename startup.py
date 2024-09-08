cf0f638849bc77d828e18422358616b6da2560ac
neastman018 <neastman018@vt.edu>
2024-09-08 14:28:22 -0400

    startup update

--- startup.py
+++ startup.py
@@ -6,10 +6,10 @@ def run_script(script_name):
 
 if __name__ == "__main__":
 
-    run("WAYLAND_DISPLAY='wayland-1' wlr-randr --output HDMI-A-1 --transform 270", shell=True)
-
     t1 = Thread(target=run_script, args=("python backend/app/main.py",))
     t2 = Thread(target=run_script, args=("cd frontend && npm run start",))
+    run("WAYLAND_DISPLAY='wayland-1' wlr-randr --output HDMI-A-1 --transform 270", shell=True)
+
 
     t1.start()
     t2.start()
