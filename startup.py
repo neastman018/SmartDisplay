e1e09dd87a8db86b5306d6fa54f6967c4a74ccd7
neastman018 <neastman018@vt.edu>
2024-09-08 14:12:37 -0400

    startup update

--- startup.py
+++ startup.py
@@ -2,5 +2,5 @@ from subprocess import run
 
 
 run("WAYLAND_DISPLAY='wayland-1' wlr-randr --output HDMI-A-1 --transform 270", shell=True)
-run("cd backend && python3 app/main.py", shell=True)
+run("python3 backend/app/main.py", shell=True)
 run("cd frontend && npm run start", shell=True)
