c58f07de1bd12943e3d6ad98e7c30e8f14706981
neastman018 <neastman018@vt.edu>
2024-09-14 08:44:30 -0400

    startup

--- startup.py
+++ startup.py
@@ -8,10 +8,11 @@ if __name__ == "__main__":
 
     t1 = Thread(target=run_script, args=("python backend/app/main.py",))
     t2 = Thread(target=run_script, args=("cd frontend && npm run start",))
-    run("WAYLAND_DISPLAY='wayland-1' wlr-randr --output HDMI-A-1 --transform 270", shell=True)
-
 
     t1.start()
     t2.start()
     t1.join()
     t2.join()
+
+    
+
