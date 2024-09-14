d1de849eddc83ffeb31bdfcdf705a8f96675b99b
neastman018 <neastman018@vt.edu>
2024-09-14 19:23:24 -0400

    change

--- backend/app/main.py
+++ backend/app/main.py
@@ -46,7 +46,7 @@ button2.init_button()
 print("Running")
 #morning_alarm.play_alarm()
 while True:
-    morning_alarm.activate(19, 17)
+    morning_alarm.activate(19, 25)
     print("State is", state)
 
     match state:
