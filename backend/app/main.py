feafbd6bbb03117156c4c0e13dd33064610a225a
neastman018 <neastman018@vt.edu>
2024-09-14 08:37:51 -0400

    commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,16 +5,26 @@ import RPi.GPIO as GPIO
 import time
 from datetime import datetime
 from subprocess import run
+from enum import Enum
 
 
 
 PIN1= 6
 PIN2 = 5
 
-display = Display()
+class States(Enum):
+    DEFAULT = 0
+    SLEEP = 1
+    WAKE = 2
+    ALARM = 3
+    
 
-# morning_alarm = Alarm()
-# morning_alarm.init("Good_MorningV2.mp3")
+
+display = Display()
+state = States.DEFAULT
+
+morning_alarm = Alarm()
+morning_alarm.init("Good_MorningV2.mp3")
 
 
 GPIO.setwarnings(False)
@@ -29,24 +39,53 @@ run("WAYLAND_DISPLAY='wayland-1' wlr-randr --output HDMI-A-1 --transform 270", s
 print("Running")
 #morning_alarm.play_alarm()
 while True:
+    morning_alarm.activate(8, 39)
 
-    # if not morning_alarm.is_active():
-    #     morning_alarm.activate(23, 48)
+    match state:
+        # screen and leds on are on alarm is not playing
+        case States.DEFAULT:
+            if button1.press(): # display and leds turn off
+                states = States.SLEEP
+                print("Button 1 pressed: Display is turning off")
+                display.turn_off_display()
+                time.sleep(0.5)
+
+            elif morning_alarm.is_active():
+                states = States.ALARM
+                print("Alarm is playing")
+                time.sleep(0.5)
+
+        # screen and leds are off       
+        case States.SLEEP:
+            if button1.press():
+                states = States.DEFAULT
+                print("Button 1 pressed: Display is turning on")
+                display.turn_on_display()
+                time.sleep(0.5)
+            
+            
+        case States.WAKE:
+            print("Wake State")
+
+        # Alarm is playing    
+        case States.ALARM:
+
+            if button2.press():
+                morning_alarm.alarm_stop()
+                states = States.DEFAULT
+                print("Alarm stopped")
+                time.sleep(0.5)
+            
+
+    # if button1.press() and display.state:
+    #     print("Button 1 pressed: Display is turning off")
+    #     display.turn_off_display()
+    #     time.sleep(0.5)
+
+    # elif button1.press() and not display.state:
+    #     print("Button 1 pressed: Display is turning on")
     #     display.turn_on_display()
-
-
-    # if button2.press() and morning_alarm.is_active():
-    #     morning_alarm.alarm_stop()
-
-    if button1.press() and display.state:
-        print("Button 1 pressed: Display is turning off")
-        display.turn_off_display()
-        time.sleep(0.5)
-
-    elif button1.press() and not display.state:
-        print("Button 1 pressed: Display is turning on")
-        display.turn_on_display()
-        time.sleep(0.5)
+    #     time.sleep(0.5)
 
 
     
