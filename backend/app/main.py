b57659d074436633bf7bcc8a78d175993fb95b4c
neastman018 <neastman018@vt.edu>
2024-11-02 23:57:18 -0400

    change time

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ class States(Enum):
     WAKE = 2
     ALARM = 3
 
-wake_up_times = ["7:30", "6:15", "6:15", "6:15", "6:15", "6:15", "23:55"]    
+wake_up_times = ["7:30", "6:15", "6:15", "6:15", "6:15", "6:15", "23:59"]    
 
 
 display = Display()
