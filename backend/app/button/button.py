665fbc8a2883ebb03a43c2a4c1588ad61b242741
neastman018 <neastman018@vt.edu>
2025-07-27 21:29:57 -0400

    pot button fix

--- backend/app/button/button.py
+++ backend/app/button/button.py
@@ -11,12 +11,15 @@ class Button:
     @parameter state is whichever state variable the button is controlling
     @parameter last_press in the time of the last button press for debounce purposes
     """
-    def __init__(self, pin = 0, state = False, pressed = False, last_press = time.time(), debounce : float  = 0.5):
+    def __init__(self, pin = 0, state = False, pressed = False, triggered = False, last_triggered = time.time(), last_press = time.time(), debounce : float  = 0.1, hold : float = 0.01):
         self.pin = pin
         self.state = state
         self.pressed = pressed
+        self.triggered = triggered # Will help filter out false positives
+        self.last_triggered = last_triggered
         self.last_press = last_press
         self.debounce = debounce
+        self.hold = hold
         self.button = None
         """
         Method to inialize the button
@@ -27,9 +30,17 @@ class Button:
 
 
     def press(self) -> bool:
+        if time.time() - self.last_triggered > self.hold:
+            self.triggered = False
+
         if GPIO.input(self.pin) == GPIO.HIGH and (time.time() - self.last_press) >= self.debounce and not self.pressed:
-            self.pressed = True
-            self.last_press = time.time()
+            if (time.time() - self.last_triggered) >= self.hold and not self.triggered:
+                self.last_triggered = True
+            
+            elif self.triggered:
+                self.pressed = True
+                self.last_press = time.time()
+                self.triggered = False
 
         elif GPIO.input(self.pin) == GPIO.LOW and (time.time() - self.last_press) >= self.debounce and self.pressed:
             self.pressed = False
